ada8ac08f6a73b4dad861427a25972e74a07293a
TorhamDEV <torhamdev@gmail.com>
2021-11-02 19:59:15 +0330

    get ping and return ip

---
+++ tools/ping.py
@@ -0,0 +1,26 @@
+import subprocess
+import platform
+
+
+def ping_clear(ping_output):
+    """
+    clear ping function output and return ip 
+    """
+    if platform.system().lower()=='windows': 
+        ip = ping_output.split(" ")[2].replace("[","").replace("]","") 
+    else:
+        ip = ping_output.split(" ")[2].replace("(","").replace(")","")
+
+    return ip
+
+def ping(host):
+    """
+    Use the ping command  for get target ip
+    """
+
+    # Option for the number of packets as a function of
+    param = '-n' if platform.system().lower()=='windows' else '-c'
+
+    # Building the command. Ex: "ping -c 1 google.com"
+    command = ['ping', param, '1', host]
+    return ping_clear(subprocess.check_output(command).decode())
