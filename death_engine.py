ac53c443dac296c6b74f280a9a864d018c5ff3e6
psyk3r <86204745+psyk3r@users.noreply.github.com>
2021-11-03 10:29:30 +0330

    Update death_engine.py

--- death_engine.py
+++ death_engine.py
@@ -1,3 +1,27 @@
 from tools import whois
+import os, platform
 
-whois.get_whois()
+banner()
+ch = input()
+if ch == '0':
+    whois.get_whois()
+elif ch == '1':
+    cls()
+    banner()
+else:
+    cls()
+    banner()
+
+
+
+def banner():
+    print(''' Death engine
+    [0] Whois
+    [1] none
+    ''')
+    
+def cls():
+    if platform.uname()[0] == "Linux" :
+        os.system("clear")
+    else:
+        os.system("cls")
