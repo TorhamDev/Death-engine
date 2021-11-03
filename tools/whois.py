441594824ce66eef373a7f6c7ce5ad8bb82697d7
psyk3r <86204745+psyk3r@users.noreply.github.com>
2021-11-03 11:23:50 +0330

    Update whois.py

--- tools/whois.py
+++ tools/whois.py
@@ -1,7 +1,7 @@
-from ping import ping_clear
+from .ping import ping_clear
 from ipwhois import IPWhois
 
 def get_whois(ip):
-    ping_clear(ip)
+    ip = ping_clear(ip)
     obj = IPWhois(ip)
     return(obj.lookup_whois())
