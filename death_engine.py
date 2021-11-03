22356c58b36b863ae0886ec4726b697d945560f2
TorhamDEV <torhamdev@gmail.com>
2021-11-03 13:39:52 +0330

    add target option and whois option

--- death_engine.py
+++ death_engine.py
@@ -1,35 +1,50 @@
-from tools import whois
+from tools import whois, ping
 import os, platform
+from optparse import OptionParser
+
+
+
+
+
+parser = OptionParser()
+
+
+parser.add_option("-t", "--target", dest="target",
+                metavar="url",help="define target")
+
+parser.add_option("-w", "--whois", dest="whois",
+                action="store_true", default=False,
+                metavar="ip",help="get whois from target with IP ")
+
 
 def banner():
-    print('''Death engine
-    [0] Whois
-    [1] none
-    ''')
+    pass
     
+
 def cls():
     if platform.uname()[0] == "Linux" :
         os.system("clear")
     else:
         os.system("cls")
 
-def mainm():
-    banner()
-    ch = input()
-    cls()
-    if ch == '0':
-        ip = input('Enter domain ot ip address : ')
-        print(whois.get_whois(ip))
-        mainm()
-    
-    elif ch == '1':
-        cls()
-        mainm()
-    
-    else:
-        cls()
-        mainm()
-cls()
-mainm()
 
+
+def main():
+    (options, args) = parser.parse_args()
+    print(options)
+    
+    if not options.target:
+        print("enter target or use -h for see help")
+        exit
+
+
+    if options.target:
+        target = str(options.target).replace("https://","").replace("http://","").replace("/","")
+        target_ip = ping.ping(target)
+        print(target_ip)
+
+    if options.whois:
+        print(whois.get_whois(target_ip))
+
+main()
 
