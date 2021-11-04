26962126cad1fe7531310824fbdd6b2bc4ced625
TorhamDEV <torhamdev@gmail.com>
2021-11-04 13:10:44 +0330

    Trying to correct the reflection

--- tools/whois.py
+++ tools/whois.py
@@ -2,7 +2,8 @@
 from .ping import ipfind
 from ipwhois import IPWhois
 
-def get_whois(ip):
+def get_whois(target_ip):
+    ip = ipfind(target_ip)
     obj = IPWhois(ip)
     return obj.lookup_whois()
 
