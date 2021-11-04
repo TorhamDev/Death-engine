67984f976f6958b36307e15b159ef3ca7528a398
psyk3r <86204745+psyk3r@users.noreply.github.com>
2021-11-04 11:52:46 +0330

    Update ping.py

--- tools/ping.py
+++ tools/ping.py
@@ -3,21 +3,9 @@ import platform
 import socket
 
 
-def ping_clear(ip):
+def ipfind(target_ip):
     """
-    clear ping function output and return ip 
+    Use the gethostbyname method to get ip address
     """
-    ip = socket.gethostbyname(ip)
-    return ip
-
-def ping(host):
-    """
-    Use the ping command  for get target ip
-    """
-
-    # Option for the number of packets as a function of
-    param = '-n' if platform.system().lower()=='windows' else '-c'
-
-    # Building the command. Ex: "ping -c 1 google.com"
-    command = ['ping', param, '1', host]
-    return ping_clear(subprocess.check_output(command).decode())
+    ip = socket.gethostbyname(target_ip)
+    return(ip)
