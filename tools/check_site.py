e53dc04b6b86f32df4e791b9c00c2d173b964968
torhamdev <torhamdev@gmail.com>
2022-07-02 19:58:04 +0430

    Imporove site check

--- tools/check_site.py
+++ tools/check_site.py
@@ -10,9 +10,13 @@ def site_is_up(url):
             return(True)
 
         else:
-            print("target not available")
-            print("site is down, status code: ", code)
-            return(False)
+            code = session.get(f"http://{url}", verify=False).status_code
+            if code == 200:
+                return(True)
+            else:
+                print("target not available")
+                print("site is down, status code: ", code)
+                return(False)
 
     except Exception as e:
         print("target not available")
